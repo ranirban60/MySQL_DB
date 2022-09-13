0522f4ee14854c0b7b10b8e72a3c8f31afefdba0
ranirban60 <64977972+ranirban60@users.noreply.github.com>
2022-09-13 19:08:38 +0530

    Added: Code for Insert the employee payroll data

---
+++ UC3_CreateEmployeePayrollData.sql
@@ -0,0 +1,3 @@
+mysql> INSERT INTO employee_payroll (name, salary, start_date) VALUES ('Devid',30000,'2022-07-05'),('Prince',35000,'2021-08-30'),('Diana',37000,'2020-10-05'),('Warner',33000,'2019-09-29'),('Jack',50000,'2018-01-05');
+Query OK, 5 rows affected (0.01 sec)
+Records: 5  Duplicates: 0  Warnings: 0
