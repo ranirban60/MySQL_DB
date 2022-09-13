761501eb92a3482599e0e5bc6fef489125ff1a5a
ranirban60 <64977972+ranirban60@users.noreply.github.com>
2022-09-13 18:55:02 +0530

    Update and rename MySQL_DB.sql to UC1_CreatePayrollServiceDB.sql
    Added: Create payroll service Database

---
+++ UC1_CreatePayrollServiceDB.sql
@@ -0,0 +1,20 @@
+mysql> CREATE DATABASE payroll_service;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> SHOW DATABASES;
++--------------------+
+| Database           |
++--------------------+
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| test_db_1          |
+| world              |
++--------------------+
+8 rows in set (0.00 sec)
+
+mysql> USE payroll_service;
+Database changed
